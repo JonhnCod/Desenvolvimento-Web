2c19a3d44e275abfc44a93cb8b5f9ef37dc6c8a1
Jonathan Arruda <jonhnarruda@outlook.com>
2025-05-15 08:59:49 -0300

    Ajustantado o formulario

--- PYTHON/Primeiro-site-flask/forms.py
+++ PYTHON/Primeiro-site-flask/forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email,length,EqualTo
+from wtforms.validators import DataRequired, Email,Length,EqualTo
 
 
 class LoginForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(),length(6, 20)])
-    remember_me = BooleanField('Remember Me')
-    submit_login = SubmitField('Sign In')
+    password = PasswordField('Password', validators=[DataRequired(),Length(6, 20)])
+    remember_me = BooleanField('Lembrar-me')
+    submit_login = SubmitField('Entrar')
 
 
 class RegisterForm(FlaskForm):
     username = StringField('Nome do usuário', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(),length(6,20)])
+    senha = PasswordField('Senha', validators=[DataRequired(),Length(6,20)])
     senha_confirm = PasswordField('Confirmacao da senha', validators=[DataRequired(), EqualTo('senha')])
     submit_cadastro = SubmitField('Criar conta')
 
